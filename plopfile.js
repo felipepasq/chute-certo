a36e7003ca05bc6298c737d3b6ffe097f9387d3e
Felipe Pasqua <felipepasquarodrigues@gmail.com>
2023-01-16 15:18:53 -0300

    Adding plop

---
+++ plopfile.js
@@ -0,0 +1,19 @@
+module.exports = function (plop) {
+  plop.setGenerator('component', {
+    description: 'Create a component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is this component's name?"
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'templates/Component.tsx.hbs'
+      }
+    ]
+  })
+}
